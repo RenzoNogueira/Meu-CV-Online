8e4542d34d27ef38ebae1a90e5429cc855e00dd7
RenzoNogueira <mainrenzossn@gmail.com>
2022-01-13 08:26:16 -0300

    host editado

--- php/host/conexao.php
+++ php/host/conexao.php
@@ -1,8 +1,8 @@
 <?php
 
 define('HOST', '127.0.0.1'); // Constante
-define('USUARIO', 'root'); // Mudar
-define('SENHA', ''); // Mudar
+define('USUARIO', 'u914795534_renzonogueiras'); // Mudar
+define('SENHA', 'W0#Z4&7Nlz'); // Mudar
 define('DB', 'u914795534_rzssn');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Erro ao conectar, tente novamente mais tarde ou recarregue a página.');
