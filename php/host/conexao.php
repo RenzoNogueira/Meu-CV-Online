5fe2ecd8136f1d85a94b0b035c5e949db976acba
RenzoNogueira <46146183+RenzoNogueira@users.noreply.github.com>
2022-03-07 08:56:55 -0300

    conexão reconfigurada
    conexão reconfigurada

--- php/host/conexao.php
+++ php/host/conexao.php
@@ -2,9 +2,12 @@
 if (!isset($_SESSION))  session_start();
 $_SESSION["user"] = false;
 define('HOST', '127.0.0.1'); // Constante
-define('USUARIO', 'root'); // Mudar
-define('SENHA', ''); // Mudar
+define('USUARIO', 'u914795534_renzonogueiras'); // Mudar
+define('SENHA', 'W0#Z4&7Nlz'); // Mudar
 define('DB', 'u914795534_rzssn');
+// define('USUARIO', 'root'); // Mudar
+// define('SENHA', ''); // Mudar
+// define('DB', 'u914795534_rzssn');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Erro ao conectar, tente novamente mais tarde ou recarregue a página.');
 $conexao->set_charset('utf8');
